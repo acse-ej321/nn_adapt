1648a9b92aac0942f5a6b1d3ea3f3d0f216ce216
acse-ej321 <89605848+acse-ej321@users.noreply.github.com>
2025-01-14 11:28:45 +0000

    update convergence run in makefile

--- scripts/turbine_run_convergence.py
+++ scripts/turbine_run_convergence.py
@@ -6,22 +6,25 @@ from models import TurbineMeshSeq
 Model = TurbineMeshSeq
 
 # EDIT THE ROOT DIRECTORY
-rootfolder = "/home/phd01/00_models/nn_adapt/output/"
+# rootfolder = "/home/phd01/00_models/nn_adapt/output/"
 # rootfolder = f"/data0/nn_adapt/output/""
+rootfolder = f"/home/ej321/output"
 
 # EDIT THE MAIN SIMULATION FOLDER
-subfolder = rootfolder+f"E2N_compare_convergence/"
+subfolder = rootfolder+f"E2N_compare_Dec2025"
 
-num_refinements = 15
+num_refinements = 40
 # num_refinements = 2 # testing
-f=2
-for i in range(num_refinements+1):
+f=0.5
+for i in range(2,num_refinements+1):
     target_complexity = 100.0 * (2**(f * i))
-    print(f'\n\n *** convergence loop {i, target_complexity} *** \n\n')\
+    print(f'\n\n *** convergence loop {i, target_complexity} *** \n\n')
     input_params = {
-        "target": target_complexity,
+        "target": target_complexity
         }
-    sim_uniform = Simulation(Model, subfolder, parameters = input_params)
+    print(f"input parameters: {input_params}")
+    sim_uniform = Simulation(Model, parameters = input_params)
+    # sim_uniform = Simulation(Model, subfolder, parameters = input_params)
     print(f'\n\nmethod: {sim_uniform.params["adaptor_method"] },\
     {sim_uniform.params["indicator_method"]} \n\n')
     logging.info(sim_uniform.params)
--- scripts/turbine_run_forward.py
+++ scripts/turbine_run_forward.py
@@ -6,12 +6,14 @@ from models import TurbineMeshSeq
 Model = TurbineMeshSeq
 
 # EDIT THE ROOT DIRECTORY
-rootfolder = "/home/phd01/00_models/nn_adapt/output/"
+# rootfolder = "/home/phd01/00_models/nn_adapt/output/"
 # rootfolder = f"/data0/nn_adapt/output/""
+rootfolder = f"/home/ej321/output"
 
 # EDIT THE MAIN SIMULATION FOLDER
-subfolder = rootfolder+f"forward_test/"
+subfolder = rootfolder+f"E2N_compare_Dec2025"
 
+# sim_uniform = Simulation(Model)
 sim_uniform = Simulation(Model, subfolder)
 print(f'\n\nmethod: {sim_uniform.params["adaptor_method"] },\
  {sim_uniform.params["indicator_method"]} \n\n')
